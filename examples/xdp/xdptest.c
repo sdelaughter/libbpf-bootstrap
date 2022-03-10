0751e7b12fb8a1cd9da6e1e623e4ec2ef2d3e144
Samuel DeLaughter <samdelaughter@gmail.com>
2022-03-10 11:18:08 -0500

    migrate xdp examples to separate folder

--- examples/xdp/xdptest.c
+++ examples/xdp/xdptest.c
@@ -30,32 +30,23 @@ static const struct argp_option opts[] = {
 	{},
 };
 
-static int bpf_object__attach_skeleton_xdp(struct bpf_object_skeleton *s, int ifindex)
-{
-	int i, err;
-
-	for (i = 0; i < s->prog_cnt; i++) {
-		struct bpf_program *prog = *s->progs[i].prog;
-		struct bpf_link **link = s->progs[i].link;
-
-		// if (!prog->load)
-		// 	continue;
-		//
-		// /* auto-attaching not supported for this program */
-		// if (!prog->sec_def || !prog->sec_def->attach_fn)
-		// 	continue;
-
-		*link = bpf_program__attach_xdp(prog, ifindex);
-		err = libbpf_get_error(*link);
-		if (err) {
-			// pr_warn("failed to auto-attach program '%s': %d\n",
-			// 	bpf_program__name(prog), err);
-			return err;
-		}
-	}
-
-	return 0;
-}
+// static int bpf_object__attach_skeleton_xdp(struct bpf_object_skeleton *s, int ifindex)
+// {
+// 	int i, err;
+//
+// 	for (i = 0; i < s->prog_cnt; i++) {
+// 		struct bpf_program *prog = *s->progs[i].prog;
+// 		struct bpf_link **link = s->progs[i].link;
+//
+// 		*link = bpf_program__attach_xdp(prog, ifindex);
+// 		err = libbpf_get_error(*link);
+// 		if (err) {
+// 			return err;
+// 		}
+// 	}
+//
+// 	return 0;
+// }
 
 static error_t parse_arg(int key, char *arg, struct argp_state *state)
 {
@@ -157,7 +148,15 @@ int main(int argc, char **argv)
 	// 	fprintf(stderr, "Failed to attach BPF skeleton\n");
 	// 	goto cleanup;
 	// }
-	err = bpf_object__attach_skeleton_xdp(skel->skeleton, env.ifindex);
+
+	struct bpf_program *prog = *skel->skeleton->progs[0].prog;
+	struct bpf_link **link = skel->skeleton->progs[0].link;
+	*link = bpf_program__attach_xdp(prog, ifindex);
+	err = libbpf_get_error(*link);
+	// if (err) {
+	// 	return err;
+	//
+	// err = bpf_object__attach_skeleton_xdp(skel->skeleton, env.ifindex);
 	if (err) {
 		fprintf(stderr, "Failed to attach BPF skeleton\n");
 		goto cleanup;
