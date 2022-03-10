cf385c5f0ae5a10e6aafd7d38147c4b77e7b1e72
Samuel DeLaughter <samdelaughter@gmail.com>
2022-03-10 12:47:17 -0500

    debugging

--- examples/xdp/parse.bpf.c
+++ examples/xdp/parse.bpf.c
@@ -5,6 +5,7 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include <linux/bpf.h>
 #include "parse.h"
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
