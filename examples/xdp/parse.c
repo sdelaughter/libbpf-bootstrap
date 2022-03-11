28d5e95a6473e684c6877437394db68acdfed6b3
Samuel DeLaughter <samdelaughter@gmail.com>
2022-03-11 14:51:02 -0500

    bug fix

--- examples/xdp/parse.c
+++ examples/xdp/parse.c
@@ -131,7 +131,7 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
 	char *proto_name_icmp = "ICMP";
 	char *proto_name_udp = "UDP";
 	char *proto_name_tcp = "TCP";
-	char *proto_name_tcp = "GRE";
+	char *proto_name_gre = "GRE";
 	char *proto_name_other = "UNKNOWN";
 
 	char *eth_protocol;
