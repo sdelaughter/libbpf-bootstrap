608bbf1412ead38782599493afafac7d1ca9505f
Samuel DeLaughter <samdelaughter@gmail.com>
2022-03-14 17:12:59 -0400

    debugging

--- examples/xdp/syn_prover.bpf.c
+++ examples/xdp/syn_prover.bpf.c
@@ -103,7 +103,7 @@ static void do_syn_pow(struct iphdr* iph, struct tcphdr* tcph, struct event* e){
 	digest.seq = tcph->seq;
 
 	#pragma unroll
-	for (int i=0; i<POW_ITERS; i++) {
+	for (unsigned int i=0; i<POW_ITERS; i++) {
 		e->hash_iters = i+1;
 		digest.ack_seq = nonce + i;
 		hash = syn_hash(&digest);
