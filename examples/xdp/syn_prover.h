63836ff07915762724b4481889f2d25ff11d099e
Samuel DeLaughter <samdelaughter@gmail.com>
2022-03-14 16:15:09 -0400

    debugging

--- examples/xdp/syn_prover.h
+++ examples/xdp/syn_prover.h
@@ -4,9 +4,15 @@
 #ifndef __SYN_PROVER_H
 #define __SYN_PROVER_H
 
-const int POW_ITERS = 10;
+const unsigned long ETH_P_IP = 2048;
+const unsigned long ETH_P_IPV6 = 34525;
 
-#include "syn_pow.h"
+const unsigned long long POW_THRESHOLD  = 0;
+// const unsigned long long POW_THRESHOLD  = 2147483648; // k=2
+// const unsigned long long POW_THRESHOLD  = 3221225472; // k=4
+// const unsigned long long POW_THRESHOLD  = 3758096384; // k=8
+
+const int POW_ITERS = 10;
 
 struct event {
 	unsigned long long start_ts;
--- examples/xdp/syn_verify.h
+++ examples/xdp/syn_verify.h
@@ -4,7 +4,13 @@
 #ifndef __SYN_VERIFY_H
 #define __SYN_VERIFY_H
 
-#include "syn_verify.h"
+const unsigned long ETH_P_IP = 2048;
+const unsigned long ETH_P_IPV6 = 34525;
+
+const unsigned long long POW_THRESHOLD  = 0;
+// const unsigned long long POW_THRESHOLD  = 2147483648; // k=2
+// const unsigned long long POW_THRESHOLD  = 3221225472; // k=4
+// const unsigned long long POW_THRESHOLD  = 3758096384; // k=8
 
 struct event {
 	unsigned long long start_ts;
