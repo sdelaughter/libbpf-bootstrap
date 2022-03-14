a3322506f585fc461bad8cad26905bdd2587940c
Samuel DeLaughter <samdelaughter@gmail.com>
2022-03-14 18:33:54 -0400

    k=16, iters=100

--- examples/xdp/syn_prover.h
+++ examples/xdp/syn_prover.h
@@ -7,12 +7,15 @@
 const unsigned long ETH_P_IP = 2048;
 const unsigned long ETH_P_IPV6 = 34525;
 
-// const unsigned long POW_THRESHOLD  = 0;
+// const unsigned long POW_THRESHOLD  = 0; // k=1
 // const unsigned long POW_THRESHOLD  = 2147483648; // k=2
 // const unsigned long POW_THRESHOLD  = 3221225472; // k=4
-const unsigned long POW_THRESHOLD  = 3758096384; // k=8
+// const unsigned long POW_THRESHOLD  = 3758096384; // k=8
+const unsigned long POW_THRESHOLD  = 4026531840; // k=16
+// const unsigned long POW_THRESHOLD  = 4160749568; // k=32
+// const unsigned long POW_THRESHOLD  = 4227858432; // k=64
 
-const int POW_ITERS = 50;
+const int POW_ITERS = 100;
 
 struct event {
 	unsigned long long start_ts;
