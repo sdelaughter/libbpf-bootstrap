b71e3151a75688a96431399c0e387bca5dda9afe
Samuel DeLaughter <samdelaughter@gmail.com>
2022-05-19 14:24:38 -0400

    debugging

--- examples/xdp/hello.c
+++ examples/xdp/hello.c
@@ -38,18 +38,18 @@ static int bpf_object__attach_skeleton_net(struct bpf_object_skeleton *s) {
 		struct bpf_program *prog = *s->progs[i].prog;
 		struct bpf_link **link = s->progs[i].link;
 
-		// if (!prog->load)
-		// 	continue;
-		//
-		// /* auto-attaching not supported for this program */
-		// if (!prog->sec_def || !prog->sec_def->attach_fn)
-		// 	continue;
+		if (!prog->load)
+			continue;
+
+		/* auto-attaching not supported for this program */
+		if (!prog->sec_def || !prog->sec_def->attach_fn)
+			continue;
 
 		*link = bpf_program__attach_tracepoint(prog, "net", "net_dev_xmit");//, ifindex);
 		err = libbpf_get_error(*link);
 		if (err) {
-			// pr_warn("failed to auto-attach program '%s': %d\n",
-			// 	bpf_program__name(prog), err);
+			pr_warn("failed to auto-attach program '%s': %d\n",
+				bpf_program__name(prog), err);
 			return err;
 		}
 	}
