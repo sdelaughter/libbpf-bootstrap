c386261743e32e5419fd1fe424cef98e61f5ec41
Samuel DeLaughter <samdelaughter@gmail.com>
2022-05-26 10:31:17 -0400

    debugging

--- examples/xdp/bootstrap.h
+++ examples/xdp/bootstrap.h
@@ -3,6 +3,9 @@
 #ifndef __BOOTSTRAP_H
 #define __BOOTSTRAP_H
 
+const unsigned long ETH_P_IP = 2048;
+const unsigned long ETH_P_IPV6 = 34525;
+
 struct event {
 	unsigned long long start;
 	unsigned long long end;
