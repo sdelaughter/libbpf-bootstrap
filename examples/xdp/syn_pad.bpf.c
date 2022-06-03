829085d3cb4c9280546d0afed8912b07cab43ca4
Samuel DeLaughter <samdelaughter@gmail.com>
2022-06-03 12:51:58 -0400

    debug

--- examples/xdp/syn_pad.bpf.c
+++ examples/xdp/syn_pad.bpf.c
@@ -47,18 +47,17 @@ void bpf_memset(void *s, int c,  unsigned int len){
     while(len--) {
         *p++ = (unsigned char)c;
     }
-    return s;
 }
 
 void bpf_memmove_left(void *dst, void * src, unsigned int len){
 	for (int i=len-1; i >= 0; i--) {
-		(unsigned char *)dst + i = (unsigned char) *(src+i)
+		(unsigned char *)dst + i = (unsigned char) *((unsigned char *)src+i)
 	}
 }
 
 void bpf_memmove_right(void *src, void *dst, unsigned int len){
     for (int i=0; i<len; i++) {
-				(unsigned char *)dst + i = (unsigned char) *(src+i)
+				(unsigned char *)dst + i = (unsigned char) *((unsigned char *)src+i)
     }
 }
 
