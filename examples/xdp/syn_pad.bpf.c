46cc547bdd3082d05870b67638ab8204bdd31a62
Samuel DeLaughter <samdelaughter@gmail.com>
2022-06-09 10:55:06 -0400

    debug

--- examples/xdp/syn_pad.bpf.c
+++ examples/xdp/syn_pad.bpf.c
@@ -69,48 +69,7 @@ static __always_inline void set_ip_csum(struct iphdr* iph){
   iph->check = csum((unsigned short*)iph, iph->ihl<<2);
 }
 
-static __always_inline void set_tcp_csum(struct iphdr *iph, unsigned short *ipPayload) {
-	//From https://gist.github.com/david-hoze/0c7021434796997a4ca42d7731a7073a
-  register unsigned long sum = 0;
-  unsigned short tcpLen = bpf_ntohs(iph->tot_len) - (iph->ihl<<2);
-	struct tcphdr *tcph = (struct tcphdr*)(ipPayload);
-  //add the pseudo header
-  //the source ip
-  sum += (iph->saddr>>16)&0xFFFF;
-  sum += (iph->saddr)&0xFFFF;
-  //the dest ip
-  sum += (iph->daddr>>16)&0xFFFF;
-  sum += (iph->daddr)&0xFFFF;
-  //protocol and reserved: 6
-  sum += bpf_htons(IPPROTO_TCP);
-  //the length
-  sum += bpf_htons(tcpLen);
-
-  //add the IP payload
-  //initialize checksum to 0
-  tcph->check = 0;
-  while (tcpLen > 1) {
-	  sum += * ipPayload++;
-	  tcpLen -= 2;
-  }
-
-	//if any bytes left, pad the bytes and add
-  if(tcpLen > 0) {
-    //printf("+++++++++++padding, %dn", tcpLen);
-    sum += ((*ipPayload)&bpf_htons(0xFF00));
-  }
-
-  //Fold 32-bit sum to 16 bits: add carrier to result
-  while (sum>>16) {
-    sum = (sum & 0xffff) + (sum >> 16);
-  }
-  sum = ~sum;
-
-	//set computation result
-  tcph->check = (unsigned short)sum;
-}
-
-static __always_inline void set_tcp_csum2(struct iphdr *iph, struct tcphdr *tcph) {
+static __always_inline void set_tcp_csum(struct iphdr *iph, struct tcphdr *tcph) {
 	//From https://gist.github.com/david-hoze/0c7021434796997a4ca42d7731a7073a
   register unsigned long sum = 0;
   unsigned short tcpLen = bpf_ntohs(iph->tot_len) - (iph->ihl<<2);
@@ -151,26 +110,6 @@ static __always_inline void set_tcp_csum2(struct iphdr *iph, struct tcphdr *tcph
 }
 
 
-//
-// void bpf_memset(void *s, int c,  unsigned int len){
-//     unsigned char* p=s;
-//     while(len--) {
-//         *p++ = (unsigned char)c;
-//     }
-// }
-
-// void bpf_memmove_left(void *dst, void * src, unsigned int len){
-// 	for (int i=len-1; i >= 0; i--) {
-// 		(unsigned char *)dst + i = (unsigned char) *((unsigned char *)src+i)
-// 	}
-// }
-//
-// void bpf_memmove_right(void *src, void *dst, unsigned int len){
-//     for (int i=0; i<len; i++) {
-// 				(unsigned char *)dst + i = (unsigned char) *((unsigned char *)src+i)
-//     }
-// }
-
 SEC("xdp")
 int xdp_pass(struct xdp_md *ctx) {
 	bool found_syn = false;
@@ -205,28 +144,8 @@ int xdp_pass(struct xdp_md *ctx) {
 								if (bpf_xdp_adjust_tail(ctx, padding_needed)) {
 										return XDP_PASS;
 								}
-								// if ((void *)tcp + sizeof(*tcp) + n_tcp_op_bytes < data_end) {
-		            //   char *payload = (void *)tcp + sizeof(*tcp) + n_tcp_op_bytes;
-								// 	size_t payload_size = iph->tot_len - (sizeof(struct iphdr) + sizeof(struct tcphdr));
-								// 	// bpf_memmove_right((void *)payload + padding_needed, (void *) payload, payload_size);
-		            // }
-								// if (padding_needed > 1) {
-								// 	bpf_memset((void * )payload, NO_OP_VAL, padding_needed - 1);
-								// }
-								// bpf_memset((void * )payload + (padding_needed - 1), END_OP_VAL, 1);
-								tcph->doff = SYN_PAD_MIN_DOFF;
 								padding_added = padding_needed;
-
-								set_tcp_csum2(iph, tcph);
-								set_ip_csum(iph);
-
-								// unsigned char *padding = (void *)tcph + sizeof(*tcph);
-								//
-								// #pragma unroll
-								// for (size_t i = 0; i < padding_added; i++) {
-								// 	padding[i] = NO_OP_VAL;
-								// }
-								// padding[padding_needed - 1] = NO_OP_VAL;
+								unsigned char *padding = tcph + sizeof(*tcph) + n_tcp_op_bytes;
 							}
 						}
 					}
@@ -235,24 +154,36 @@ int xdp_pass(struct xdp_md *ctx) {
 		}
 	}
 
-	#if GENERATE_EVENTS
-		if (found_syn) {
-			#if MEASURE_TIME
-				end_time = bpf_ktime_get_ns();
-			#endif
-			struct event *e;
-			e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
-			if (!e) {
-				bpf_printk("WARNING: Failed to reserve space in ring buffer\n");
-				return XDP_PASS;
-			}
-			e->status = 0;
-			e->start = start_time;
-			e->end = end_time;
-			e->padding = padding_added;
-			bpf_ringbuf_submit(e, 0);
-		}
-	#endif
+	if (padding_added > 0) {
+		data = (void *)(long)ctx->data;
+		data_end = (void *)(long)ctx->data_end;
+		packet_size = data_end - data;
 
+		ethh = data;
+		if ((void *)ethh + sizeof(*ethh) <= data_end) {
+			if (bpf_htons(ethh->h_proto) == ETH_P_IP) {
+				// Parse IPv4 Header
+				iph = data + sizeof(*ethh);
+				if ((void *)iph + sizeof(*iph) <= data_end) {
+					if (iph->protocol == IPPROTO_TCP) {
+						// Parse TCP Header
+						tcph = (void *)iph + sizeof(*iph);
+						if ((void *)tcph + sizeof(*tcph) <= data_end) {
+							tpch->doff = SYN_PAD_MIN_DOFF;
+							if ((void *)padding + padding_added <= data_end) {
+								#pragma unroll
+								for (int i=0; i < padding_added - 1; i++) {
+									*((void *)padding + i) = NO_OP_VAL;
+								}
+								*((void *)padding + padding_added - 1) = END_OP_VAL;
+							}
+							set_tcp_csum(iph, tcph);
+							set_ip_csum(iph);
+						}
+					}
+				}
+			}
+		}
+	}
 	return XDP_PASS;
 }
