ffca884c8d4bb1909582e17e48e7ec3155c742c7
Samuel DeLaughter <samdelaughter@gmail.com>
2022-06-10 11:33:24 -0400

    debug

--- examples/xdp/syn_pad.bpf.c
+++ examples/xdp/syn_pad.bpf.c
@@ -118,13 +118,13 @@ static __always_inline bool is_syn(struct tcphdr* tcph) {
 	return (tcph->syn && !(tcph->ack) && !(tcph->fin) &&!(tcph->rst) &&!(tcph->psh));
 }
 
-// static __always_inline void update_tcp_csum(struct tcphdr* tcph, __u32 old_doff) {
-//   if (old_doff == tcph->ack_seq) return;
-//   __sum16 sum = old_doff + (~bpf_ntohs(*(unsigned short *)&tcph->doff) & 0xffff);
-//   sum += bpf_ntohs(tcph->check);
-//   sum = (sum & 0xffff) + (sum>>16);
-//   tcph->check = bpf_htons(sum + (sum>>16) + 1);
-// }
+static __always_inline void update_ip_csum(struct tcphdr* tcph, __u32 old_doff) {
+  if (old_doff == tcph->ack_seq) return;
+  __sum16 sum = old_doff + (~bpf_ntohs(*(unsigned short *)&tcph->doff) & 0xffff);
+  sum += bpf_ntohs(tcph->check);
+  sum = (sum & 0xffff) + (sum>>16);
+  tcph->check = bpf_htons(sum + (sum>>16) + 1);
+}
 
 static __always_inline uint16_t csum(unsigned short *buf, int bufsz) {
     unsigned long sum = 0;
@@ -144,7 +144,7 @@ static __always_inline uint16_t csum(unsigned short *buf, int bufsz) {
 }
 
 static __always_inline uint16_t compute_checksum(struct iphdr *iph) {
-	size_t len = (iph->ihl * 4) - 2;
+	size_t len = (iph->ihl & 0xF0) >> 4;
 	uint16_t * bytes = (uint16_t *)((void *)iph);
   register uint32_t sum = 0;
   while (len > 1) {
