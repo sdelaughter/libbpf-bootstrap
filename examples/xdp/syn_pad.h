43e48d618c289d5f4f2adc7c4598985a9ec7ca97
Samuel DeLaughter <samdelaughter@gmail.com>
2022-06-10 12:57:42 -0400

    debug

--- examples/xdp/syn_pad.h
+++ examples/xdp/syn_pad.h
@@ -8,9 +8,10 @@
 
 #define GENERATE_EVENTS 1
 #define MEASURE_TIME 1
+#define SYN_PAD_MIN_DOFF 15
 
 const unsigned int SYN_PAD_MIN_BYTES = 40;
-const unsigned int SYN_PAD_MIN_DOFF = 15;
+
 const unsigned char END_OP_VAL = 0;
 const unsigned char NO_OP_VAL = 1;
 
